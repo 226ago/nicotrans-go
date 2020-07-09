3d4e7e4f2350b2c2671966b45cd7d6e5cbd9408b
Sangha Lee <totoriato@gmail.com>
2020-07-10 04:39:06 +0900

    반환 자료형 일치하게 수정

--- pkg/certificate/install_linux.go
+++ pkg/certificate/install_linux.go
@@ -6,6 +6,6 @@ import (
 )
 
 // InstallAsRootCA 인증서를 신뢰할 수 있는 루트 인증 기관으로 설치합니다
-func InstallAsRootCA(cert *x509.Certificate) error {
-	return fmt.Errorf("윈도우 환경에서만 사용할 수 있는 메소드입니다")
+func InstallAsRootCA(cert *x509.Certificate) (bool, error) {
+	return false, fmt.Errorf("윈도우 환경에서만 사용할 수 있는 메소드입니다")
 }
