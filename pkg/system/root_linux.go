7efd58227debc155739978df7e31db5afdb6160b
Sangha Lee <totoriato@gmail.com>
2020-07-10 02:40:47 +0900

    타이포

--- pkg/system/root_linux.go
+++ pkg/system/root_linux.go
@@ -4,11 +4,10 @@ import "fmt"
 
 // HasRoot 관리자 권한이 있는지?
 func HasRoot() (bool, error) {
-	return false, fmt.Errorf("윈도우 외 플랫폼에선  지원하지 않는 메소드입니다")
+	return false, fmt.Errorf("윈도우 환경에서만 사용할 수 있는 메소드입니다")
 }
 
 // RunMeElevated 현재 프로그램을 관리자 권한으로 다시 실행합니다
 func RunMeElevated() error {
-	return fmt.Errorf("윈도우 외 플랫폼에선  지원하지 않는 메소드입니다")
+	return fmt.Errorf("윈도우 환경에서만 사용할 수 있는 메소드입니다")
 }
-
